b83adecbc280081e59685b7bb75f2d5830509754
Diego <diego.falciola@gmail.com>
2019-04-30 16:10:56 -0300

    no message

--- client/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App/App';
-
-const target = document.getElementById('root')
-const app = <App/>
-
-ReactDOM.render(app, target);
