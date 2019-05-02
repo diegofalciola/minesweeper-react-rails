3931ce22b4554f7ffd1355977e8f265eaded6e86
Diego <diego.falciola@gmail.com>
2019-05-02 06:44:26 -0300

    no message

--- client/src/_nav.js
+++ client/src/_nav.js
@@ -10,14 +10,10 @@ export default {
       },
     },
     {
-      title: true,
       name: 'Valores por Cobrar',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-      },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
-    },
+      url: '/payments',
+      icon: 'icon-drop',
+     },
     {
       title: true,
       name: 'Theme',
--- client/src/routes.js
+++ client/src/routes.js
@@ -35,10 +35,12 @@ const Typography = React.lazy(() => import('./views/Theme/Typography'));
 const Widgets = React.lazy(() => import('./views/Widgets/Widgets'));
 const Users = React.lazy(() => import('./views/Users/Users'));
 const User = React.lazy(() => import('./views/Users/User'));
+const Payments = React.lazy(() => import('./views/Payments/Payments'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/', exact: true, name: 'Home' },
+  { path: '/payments', name: 'Dashboard', component: Payments },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/theme', exact: true, name: 'Theme', component: Colors },
   { path: '/theme/colors', name: 'Colors', component: Colors },
